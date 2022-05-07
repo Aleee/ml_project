0b3581c1e459cc4ab3b82eb209816b8d6fdafcc3
Aleee <AleeeLR@yandex.ru>
2022-05-07 11:44:34 +0300

    Force CV in train module to shuffle dataset

--- src/forest_cover/train.py
+++ src/forest_cover/train.py
@@ -7,13 +7,15 @@ import mlflow.sklearn
 
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import cross_validate
+from sklearn.model_selection import KFold
+
+SCORING = ['balanced_accuracy', 'f1_weighted', 'roc_auc_ovo_weighted']
 
 
 def train(pipeline: Pipeline, data: tuple, parameters: dict, config: dict) -> dict:
-    scoring = ['balanced_accuracy', 'f1_weighted', 'roc_auc_ovo_weighted']
     with mlflow.start_run(run_name=f"{config['model']} (folds={config['eval']}, rand={config['randomstate']})"):
-        scores = cross_validate(pipeline, data[0], data[1], cv=config['eval'], scoring=scoring,
-                                return_train_score=False, error_score="raise")
+        cv_procedure = KFold(n_splits=5, shuffle=True, random_state={config['randomstate']})
+        scores = cross_validate(pipeline, data[0], data[1], cv=cv_procedure, scoring=SCORING, n_jobs=-1)
         mlflow.log_param("SCALER", config["scaler"])
         mlflow.log_param("FEATENG", config["feateng"])
         mlflow.log_param("DIMREDUCT", config["dimreduct"])
