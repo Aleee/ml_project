236270d07c9752b7cb14a8ef6009f368c50a39a0
Aleee <AleeeLR@yandex.ru>
2022-05-10 22:48:24 +0300

    Delete black from nox (no effect)

--- noxfile.py
+++ noxfile.py
@@ -8,12 +8,6 @@ def typecheck(session):
 
 
 @nox.session
-def codeformat(session):
-    session.install('black')
-    session.run('black', 'SRC')
-
-
-@nox.session
 def lint(session):
     session.install('flake8')
     session.run('flake8', 'src')
